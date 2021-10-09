84ec005deece7e641d1cc758811be204554d2bb2
soubhagya23 <ranjansoubhagya.master@gmail.com>
2021-10-09 20:06:16 +0530

    Version 1.0.3

--- src/Components/HomePage/Footer.js
+++ src/Components/HomePage/Footer.js
@@ -49,7 +49,7 @@ export default function Footer() {
           <div className="col-md-3 text-center">
 
             <a href="#" className="text-white">
-              Version:1.0.2
+              Version:1.0.3
             </a>
           </div>
         </div>
