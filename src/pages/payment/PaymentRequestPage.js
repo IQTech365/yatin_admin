7254dcc82fcc33fa40a6ee4ae52428d80c53c3db
Vijay Kumar <vijay_ku@BGMB131074.local>
2022-03-14 15:46:57 +0530

    refactor payment page

--- src/pages/payment/PaymentRequestPage.js
+++ src/pages/payment/PaymentRequestPage.js
@@ -34,6 +34,7 @@ const PaymentRequestPage = (props) => {
 
   const _handleMessage = (message) => {
     console.log("message-----", message);
+    alert(message);
     const _data = JSON.parse(message);
     setCardNumber(_data?.cardNumber);
     setExpiary(_data?.expiary);
