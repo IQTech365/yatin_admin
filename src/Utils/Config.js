2f5d235cd41436a70f8d3123377695a79c7f4425
Vijay Kumar <vijay_ku@BGMB131074.local>
2022-05-28 20:39:45 +0530

    change base url

--- src/Utils/Config.js
+++ src/Utils/Config.js
@@ -1,8 +1,8 @@
-export const url = "https://mobily-invited-server.herokuapp.com/api/";
+export const url = "https://mobility-itrade-be.herokuapp.com/api/";
 // export const url = "http://localhost:5000/api/";
 export const GoogleMapsAPI = "AIzaSyD9JwqrR1a6JaOrLVki6qvmeMNenOy7jqI";
 //https://mobily-invited-server.herokuapp.com/
-export const testmode = true;
+export const testmode = false;
 export const GiftShopCategory = [
     { id: 76, Name: "Wedding" },
     { id: 77, Name: "Birthday" },
